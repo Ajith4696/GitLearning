b1e35a199d59af2dccdd8d38511992b94f0f525a
Ajith4696 <ajithvelmurugan4696@gmail.com>
2022-03-28 11:18:08 +0530

    Tester

---
+++ src/com/git/GitAjith.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class GitAjith {
+int a=100,b=200;
+public void add() {
+System.out.println(a);
+System.out.println(b);
+}
+public static void main(String[] args) {
+	GitAjith g=new GitAjith();
+	g.add();
+	
+}
+}
